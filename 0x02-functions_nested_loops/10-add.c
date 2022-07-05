b78c6466674a4af2cf45395dc2e1002509bc0a16
Gideon Obiasor <gideonobiasor@gmail.com>
2022-07-05 15:34:24 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * add - A function that adds twonintegers and returns result
+ *
+ * @i: Input first integer
+ *
+ * @j: Input second integer
+ *
+ * Return: sum
+ */
+
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j;
+
+	return (sum);
+}
+
