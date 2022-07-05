07a34643f5049f996ee6af72dc94a59d7b730fd4
Gideon Obiasor <gideonobiasor@gmail.com>
2022-07-05 15:28:08 +0000

    Task 2, printing lowercase letters ten times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,25 +1,22 @@
 #include "main.h"
 
 /**
-* print_alphabet_10x - prints the alphabet 10 times in lower case
+* print_alphabet_x10 - prints the alphabet 10 times in lower case
 *
 * Return: nothing
 */
 
-void print_aphabet_x10(void)
+void print_alphabet_x10(void)
 {
 	char lowercase;
 	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		_putchar('\n');
-		{
 			for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
 			{
 				_putchar(lowercase);
 			}
-		}
+			_putchar('\n');
 	}
-
 }
