8db8c5aa94883ca1df681769a32e4a1b9f059069
Gideon Obiasor <gideonobiasor@gmail.com>
2022-07-06 03:00:11 -0700

    Task 0, testing the function positive_or_negative

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,3 @@
+This directory contains basic debugging tasks in C
+
+
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * positive_or_negative - Checks if a number is negative, equal to zero or positive
+ *
+ * @n: Input integer
+ *
+ * Return: nothing 
+ */
+void positive_or_negative(int i)
+{
+	if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else
+	{
+		printf("%d is positive\n", i);
+	}
+}
