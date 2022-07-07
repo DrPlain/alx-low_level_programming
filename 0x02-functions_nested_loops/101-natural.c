d7a0c9b4df5587245fed045a2f27e9c0316d205a
Gideon Obiasor <gideonobiasor@gmail.com>
2022-07-06 21:26:03 -0700

    Editing advanced task

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,27 +1,23 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
- * sumMultiples - computes and prints sum of multiples of 3 & 5 below 1024
+ * main - computes and prints sum of multiples of 3 & 5 below 1024
  *
- * Return: nothing
+ * Return: 0 )success)
  */
 
-void sumMultiples(void)
+int main(void)
 {
 	int n;
-	int sum1 = 0;
-	int sum2 = 0;
-	int sumTotal = 0;
+	int sum = 0;
 
 	for (n = 3; n < 1024; n++)
 	{
-		if (n % 3 == 0)
-			sum1 = sum1 +  n;
-		if (n % 5 == 0)
-			sum2 = sum2 +  n;
+		if (n % 3 == 0 || n % 5 == 0)
+			sum = sum +  n;
 	}
-	sumTotal = sum1 + sum2;
 
-	printf(" %d\n", sumTotal);
+	printf(" %d\n", sum);
+
+	return (0);
 }
