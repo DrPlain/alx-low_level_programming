f860d4481936f7db866c43862c6e66407a717628
Gideon Obiasor <gideonobiasor@gmail.com>
2022-07-06 20:58:34 -0700

    Advanced task: sum of all multiples of 3 or 5 below 1024

---
+++ 0x02-functions_nested_loops/101-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	sumMultiples();
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * sumMultiples - computes and prints sum of multiples of 3 & 5 below 1024
+ *
+ * Return: nothing
+ */
+
+void sumMultiples(void)
+{
+	int n;
+	int sum1 = 0;
+	int sum2 = 0;
+	int sumTotal = 0;
+
+	for (n = 3; n < 1024; n++)
+	{
+		if (n % 3 == 0)
+			sum1 = sum1 +  n;
+		if (n % 5 == 0)
+			sum2 = sum2 +  n;
+	}
+	sumTotal = sum1 + sum2;
+
+	printf("Sum of the multiples of 3 or 5 below 1024 is %d\n", sumTotal);
+}
