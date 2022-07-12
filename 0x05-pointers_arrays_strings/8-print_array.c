9aec94ba2430d0db631320ba0b3a7474948ba081
Gideon Obiasor <gideonobiasor@gmail.com>
2022-07-12 10:02:16 -0700

    Task 8, fourth commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,16 +15,11 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	if (n > 0)
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
-		{
-			if (i != n - 1)
-				printf("%d, ", a[i]);
-			else
-				printf("%d\n", a[i]);
-		}
+		if (i < n - 1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d\n", a[i]);
 	}
-	else
-		fprintf(stderr, "Invalid array size\n");
 }
