553b0f26da5ddeb4dd06f2b1ec6e4c642506ac3d
Gideon Obiasor <gideonobiasor@gmail.com>
2022-07-12 11:12:33 +0000

    Task 1, first commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * swap_int - Swaps the value of two integers)
+ *
+ * @a: First argument, integer type pointer
+ *
+ * @b: Second argument, integer type pointer
+ *
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
