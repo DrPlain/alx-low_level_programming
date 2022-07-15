5c754f8a31676a7e17488c6a59879dfd0e4e5e45
Gideon Obiasor <gideonobiasor@gmail.com>
2022-07-14 19:13:12 -0700

    Task 9, fourth commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,27 +1,29 @@
 #include "main.h"
 
-/**
- * print_number - Print a long int uaing putchar
- *
- * @n: Long int to be printed
- *
- * Return: void
- */
+void print_uint(unsigned int n);
 
+/**
+ * print_number - Prints an intege using putchar olnyr
+ * @n: The integer to print
+ */
 void print_number(int n)
 {
-	unsigned int a;
-
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-	if (n / 10 != 0)
-	{
-		a = n;
-		print_number(a / 10);
-	}
 
+	print_uint((unsigned int) n);
+}
+
+/**
+ * print_uint - Prints an unsigned integer
+ * @n: The unsigned int to print
+ */
+void print_uint(unsigned int n)
+{
+	if (n / 10 != 0)
+		print_uint(n / 10);
 	_putchar(n % 10 + '0');
 }
