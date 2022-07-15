b94ef85baf1c5d0dc6923a685fd5756e0a2c6565
Gideon Obiasor <gideonobiasor@gmail.com>
2022-07-14 18:57:20 -0700

    Task 9, third commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,6 +10,8 @@
 
 void print_number(int n)
 {
+	unsigned int a;
+
 	if (n < 0)
 	{
 		_putchar('-');
@@ -17,7 +19,8 @@ void print_number(int n)
 	}
 	if (n / 10 != 0)
 	{
-		print_number(n / 10);
+		a = n;
+		print_number(a / 10);
 	}
 
 	_putchar(n % 10 + '0');
