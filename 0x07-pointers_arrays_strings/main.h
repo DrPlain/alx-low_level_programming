eabf863db99a78eda8584bd73bf1ee32f77ad199
Gideon Obiasor <gideonobiasor@gmail.com>
2022-07-18 14:10:30 -0700

    Task 1

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -42,5 +42,6 @@ char *string_toupper(char *);
 char *leet(char *s);
 void print_number(int n);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif
