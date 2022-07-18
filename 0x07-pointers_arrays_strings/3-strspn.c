6419d3b9d9859cd43db9eb41e3de5cc261874434
Gideon Obiasor <gideonobiasor@gmail.com>
2022-07-18 15:58:57 -0700

    Task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ *
+ * @s: String to be scanned
+ *
+ * @accept: prefix substring
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int unique;
+	char *ptr = accept;
+	unsigned int count = 0;
+
+	while (*s != '\0')
+	{
+		unique = 0;
+		while (*accept != '\0')
+		{
+			if (*s == *accept)
+			{
+				count++;
+				unique = 1;
+				break;
+			}
+			accept++;
+		}
+		s++;
+		accept = ptr;
+		if (unique == 0)
+			break;
+	}
+	return (count);
+}
