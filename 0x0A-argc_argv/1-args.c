a9b3740c08e4b90faffcdc3286ba900aad3e7570
Gideon Obiasor <gideonobiasor@gmail.com>
2022-07-22 08:39:14 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - A program that prints the number of arguments
+ * passed into it.
+ *
+ * @argc: Argument count
+ *
+ * @argv: Argument vector
+ *
+ * Return: 0 Success
+ */
+
+int main(int argc, char *argv[])
+{
+
+	if (*argv[0] != '\0')
+		printf("%d\n", argc - 1);
+	return (0);
+}
