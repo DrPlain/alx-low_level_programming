34a925e4a96ede2e0a29d4238b54d0c00c49a234
Gideon Obiasor <gideonobiasor@gmail.com>
2022-07-22 09:23:51 -0700

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two intergers
+ *
+ * @argc: Argument's counter
+ *
+ * @argv: Argument's value
+ *
+ * Return: 0 success otherwise 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1;
+	int num2;
+	int result;
+
+	if (argc == 3)
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		result = num1 * num2;
+		printf("%d\n", result);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
