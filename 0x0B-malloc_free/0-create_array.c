2edf982d6db647ac5fa141c4f14a32f67ed624d5
Gideon Obiasor <gideonobiasor@gmail.com>
2022-07-24 14:44:37 -0700

    Task 0,third commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,20 +15,16 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char *array = (char *)malloc(size * sizeof(char));
+	char *array = malloc(size * sizeof(char));
+
+	if (array == NULL)
+		return (NULL);
 
 	if (size == 0)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
-	{
 		array[i] = c;
-	}
-	
-	if (array == NULL)
-		return NULL;
+
 	return (array);
-
-	free(array);
 }
-
