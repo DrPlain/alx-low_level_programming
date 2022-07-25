9c309ce49cabe17e732e352de27c206fea89cba6
Gideon Obiasor <gideonobiasor@gmail.com>
2022-07-25 13:37:59 -0700

    Task 2, seventh commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,23 +15,20 @@ int _strlen(char *s);
 
 char *str_concat(char *s1, char *s2)
 {
+	int i = 0, j = 0;
 	char *newString;
-	int i = _strlen(s1);
-	int j = _strlen(s2);
-
 
 	if (s1 == NULL)
-		s1 = "";
-
+		s1 = "\0";
 	if (s2 == NULL)
-		s2 = "";
+		s2 = "\0";
+	i = _strlen(s1);
+	j = _strlen(s2);
 
 	newString = malloc((i + j) * sizeof(*s1) + 1);
 
 	if (newString == 0)
 		return (NULL);
-
-
 	strcat(newString, s1);
 	strcat(newString, s2);
 
