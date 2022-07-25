10e18471555c0207081f8145a010a645cd33bb87
Gideon Obiasor <gideonobiasor@gmail.com>
2022-07-25 10:36:59 -0700

    Task 2, second commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,13 +24,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 		s1 = '\0';
-	else if (s2 == NULL)
+	if (s2 == NULL)
 		s2 = '\0';
-	else
-	{
-		strcat(newString, s1);
-		strcat(newString, s2);
-	}
+
+	strcat(newString, s1);
+	strcat(newString, s2);
 
 	return (newString);
 }
