426021d6be9e426a9f72085045fa2ecde6e9c096
Gideon Obiasor <gideonobiasor@gmail.com>
2022-07-28 04:58:34 -0700

    Task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "stdlib.h"
+
+/**
+ * malloc_checked - Allocates memory and on failure causes normal process teemination
+ *
+ * @b: size of memory to be allocated
+ *
+ * Return: Pointer to newly allocatef memory or exit value of 98 on failure
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *memory;
+	memory = malloc(b);
+	if (memory == NULL)
+		exit(98);
+
+	return (memory);
+}
