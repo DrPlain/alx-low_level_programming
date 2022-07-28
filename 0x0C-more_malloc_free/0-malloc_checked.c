eb61e2e34db24fe82ebcbf016bac95a78a63c7f4
Gideon Obiasor <gideonobiasor@gmail.com>
2022-07-28 05:00:28 -0700

    Task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,7 +2,8 @@
 #include "stdlib.h"
 
 /**
- * malloc_checked - Allocates memory and on failure causes normal process teemination
+ * malloc_checked - Allocates memory and on failure causes
+ * normal process termination
  *
  * @b: size of memory to be allocated
  *
@@ -12,6 +13,7 @@
 void *malloc_checked(unsigned int b)
 {
 	void *memory;
+
 	memory = malloc(b);
 	if (memory == NULL)
 		exit(98);
