2a82a0d97c39821950bb7382353a837e551991ef
Gideon Obiasor <gideonobiasor@gmail.com>
2022-08-01 14:38:15 +0000

    Task 0

---
+++ 0x0E-structures-typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - Structure describing biodata of a dog
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Name of dog's owner
+ * Description: Data structure for a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H_ */
