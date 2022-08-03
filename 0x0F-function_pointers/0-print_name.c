6739239547a81935962ed1f52554b7b9de59eb83
Gideon Obiasor <gideonobiasor@gmail.com>
2022-08-03 21:12:10 +0000

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - A function that prints name
+ *
+ * @name: Pointer to name to be printed
+ *
+ * @f: function to print name
+ *
+ * Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
