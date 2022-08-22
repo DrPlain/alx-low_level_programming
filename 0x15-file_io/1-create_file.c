9904ef6fbbe6de0192850ed3ad087bbc24283f33
Gideon Obiasor <gideonobiasor@gmail.com>
2022-08-22 20:33:18 +0000

    A function that creates a file and writes to it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	if (text_content != NULL)
-		content_len = strlen(text_content);
+		content_len = strlen(text_content) + 1;
 
 	fdo = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	fdw = write(fdo, text_content, content_len);
