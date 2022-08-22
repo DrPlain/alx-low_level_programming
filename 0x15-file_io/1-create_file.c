2de09ac6453e918b4c9e415e87128a526df90195
Gideon Obiasor <gideonobiasor@gmail.com>
2022-08-22 20:25:06 +0000

    A function that creates a file and writes to it

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * create_file - A function that creates file
+ * @filename: Name of file to create
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on success and -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fdo, fdw, content_len;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+		content_len = strlen(text_content);
+
+	fdo = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fdw = write(fdo, text_content, content_len);
+
+	if (fdo < 0 || fdw < 0)
+		return (-1);
+	close(fdo);
+
+	return (1);
+}
