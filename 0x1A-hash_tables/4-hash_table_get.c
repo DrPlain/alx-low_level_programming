b2f0a6530e82b757b38fef3ab414b2fa9b143aea
= <=>
2022-10-22 05:34:25 +0100

    Task 3

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -20,10 +20,12 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	index = key_index((const unsigned char *)key, ht->size);
 	if (index >= ht->size)
 		return (NULL);
-
 	node = ht->array[index];
-	while (node && strcmp(node->key, key) != 0)
+	while (node)
+	{
+		if (strcmp(key, node->key) == 0)
+			return (node->value);
 		node = node->next;
-
-	return ((node == NULL) ? NULL : node->value);
+	}
+	return (NULL);
 }
