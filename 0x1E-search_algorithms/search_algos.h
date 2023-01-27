aef27bb7a34d831c6c6fa07088d4ec98c9f7af60
Gideon Obiasor <gideonoiasor@gmail.com>
2023-01-27 13:23:45 +0100

    Task 1 Binary search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,5 +4,5 @@
 #include <stdio.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-
+void print_array(int *array, size_t lb, size_t ub);
 #endif
